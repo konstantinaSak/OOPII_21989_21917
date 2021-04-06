fa8838638e146bf964b9937dbc581575fd1ba12c
unknown <konstantinaSak>
2021-04-06 23:21:31 +0300

    commit Count Words

---
+++ MiddleTraveller.java
@@ -0,0 +1,11 @@
+
+// Age: (25,60] 
+public class MiddleTraveller extends Traveller {
+
+	@Override
+	protected int calculate_similarity(Object City) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ YoungTraveller.java
@@ -0,0 +1,12 @@
+
+// Age: [16,25]
+public class YoungTraveller extends Traveller {
+
+	@Override
+	protected int calculate_similarity(Object City) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+}
