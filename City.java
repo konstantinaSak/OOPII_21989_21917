b173382b56a3a5e232d304442cd8530fba2e8688
unknown <konstantinaSak>
2021-05-06 19:03:39 +0300

    One more commit

--- City.java
+++ City.java
@@ -1,10 +1,19 @@
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Comparator;
-
+import java.util.HashMap;
+import java.util.Set;
+import javax.swing.text.html.HTMLDocument.Iterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import weather.OpenWeatherMap;
 import wikipedia.MediaWiki;
+import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class City  {
 
@@ -12,7 +21,7 @@ public class City  {
 	private double[] geodesic_vector;
 	private double res;
 
-	public City() {
+	public City(String string, String appid) {
 		 terms_vector = new int[10];
 	     geodesic_vector = new double[2];
 	     res = 0;
@@ -78,7 +87,32 @@ public class City  {
 	     //System.out.println(getTerms_vector());
 	     //System.out.println(getGeodesic_vector());
 	     
-	 }
+	     ArrayList<City> cities = new ArrayList<>();
+	 	 HashMap<String,City> hm = new HashMap<String,City>();
+	 	
+	 	 for(int j=0; j<cities.size(); j++) {
+	 		if(hm.containsValue(j)) {
+	 			cities.add(j);
+	 		}
+	 	}
+	 	hm.put("City1", new City("Rome", appid));
+	 	hm.put("City2", new City("Athens",appid));
+	 	hm.put("City3", new City("Corfu",appid));
+	 	hm.put("City4", new City("Berlin",appid));
+	 	hm.put("City5", new City("Paris",appid));
+	 	hm.put("City6", new City("Thessaloniki",appid));
+	 	
+	 	Set<?> set = hm.entrySet();
+	 	Iterator i = (Iterator) set.iterator();
+	 	
+	 	while(i.hasNext()) {
+	 		@SuppressWarnings("rawtypes");
+	 		Map.Entry me = (Map.Entry)i.next();
+	 		System.out.print("key"+ me.getKey() + ".");
+	 		System.out.println("value" + ((City)me.getValue()).getMessage);
+	 	}
+	 	
+	}
 
 	public class Sortbyres implements Comparator<City>{
 	   public int compare(City a, City b){
@@ -86,12 +120,47 @@ public class City  {
 		   return (int) (a.res - b.res);
 	    }
 	}
-
+	static Connection db_can_obj = null;
+	static PreparedStatement db_prep_obj = null;
 	
-	
-	
+	private static void JDBCConnection() {
 		
-     
-
+		try {
+			Class.forName("oracle.jbdc.driver.OracleDriver");
+			System.out.println("Your oracle JBDC Driver Registered.");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Sorry couldn't find your oracle JBDC Driver.");
+			e.printStackTrace();
+			return;
+		}
+		
+		try {
+			db_can_obj = DriverManager.getConnection("jbdc:oracle:thin:@oracle12c.hua.gr:1521:orcl","21917", "21989");
+			if(db_can_obj !=null) {
+				System.out.println("Connection successful!");
+			} else {
+				System.out.println("Failed to make a connection.");
+			}
+		} catch(SQLException e) {
+			System.out.println("Oracle connection failed.");
+			e.printStackTrace();
+			return;
+		}
+	}
+	
+	private static void ReadData() throws SQLException {
+		
+		db_prep_obj = db_can_obj.prepareStatement("Select from city:");
+		ResultSet rs = db_prep_obj.executeQuery();
+		
+		while(rs.next()) {
+			 int terms vector = rs.getInt(0); // edw tha eprepe na baloume ta xarakthristika ths kathe city
+			                                  // gia na swthoun sto database.
+			
+			
+		}
+		
+	}
+	
 
 }
--- Traveller.java
+++ Traveller.java
@@ -1,68 +1,131 @@
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public abstract class Traveller extends City {
+import java.io.File;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import java.io.IOException;
+
+public abstract class Traveller extends City implements Comparable<Traveller> {
 
 	// hardcoded data
 	int prefer[] = { 10, 7, 3, 8, 0, 2, 10, 9, 5, 8 }; // 0.cafe, 1.sea, 2.museums, 3.restaurant, 4.stadium, 5.mountain,
 														// 6.airplane. 7.desert, 8.sights, 9.camping
 	double loc[] = { 37.9795, 23.7162 }; // latitude , longitude of Athens
 	int age = 18;
+	private long timestamp;
+	private double visit;
+	ArrayList<Traveller> travellers_array = new ArrayList<Traveller>();
+	JacksonTester tester = new JacksonTester();
 
-	protected abstract double calculate_similarity(Object City);
+	@Override
+	public int compareTo(Traveller traveller) {
+		return this.timestamp.compareTo(traveller.timestamp);
+	}
 
-	protected double similarity_geodesic_vector(double[] loc, double[] geodesic_vector, String unit) {
-		if ((loc[0] == geodesic_vector[0]) && loc[1] == geodesic_vector[1]) {
-			return 0;
+	public class ObjectSort(){
+		List<Traveller> travellers = new ArrayList<>();
+		travellers.add(new Traveller(0.323132));
+		Collections.sort(travellers_array);
+	}
 
-		} else {
-			double theta = loc[1] - geodesic_vector[1];
-			double distance = Math.sin(Math.toRadians(loc[0])) * Math.sin(Math.toRadians(geodesic_vector[0]))
-					+ Math.cos(Math.toRadians(loc[0])) * Math.cos(Math.toRadians(geodesic_vector[0]))* Math.cos(Math.toRadians(theta));
-			distance = Math.acos(distance);
-			distance = Math.toDegrees(distance);
-			distance = distance * 60 * 1.1515;
-			if (unit.equals("K")) {
-				distance = distance * 1.609344;
-			} else if (unit.equals("N")) {
-				distance = distance * 0.8684;
+		try
+		{
+			for (int i = 0; i < travellers_array.size(); i++) {
+				Traveller traveller;
+
+				Date date = new Date();
+				traveller.setTimestamp(date.getTime());
+				travellers_array.add(traveller);
+				Thread.sleep(i);
 			}
-			return (distance);
+			tester.writeJSON(travellers_array);
+		}catch(
+		JsonParseException e)
+		{
+			e.printStackTrace();
+		}catch(
+		JsonMappingException e)
+		{
+			e.printStackTrace();
+		}catch(
+		IOException e)
+		{
+			e.printStackTrace();
 		}
-	}
 
-	public Object compare_cities(ArrayList<City> cities) {
-		Collections.sort(cities, new Sortbyres()); // ταξινομηση στο cities με βαση το similarity 
-		System.out.println("\nSorted by similarity");
-		for (int i = 0; i < cities.size(); i++)
-			System.out.println(toString());
+        public void writeJSON(ArrayList<Traveller> arrayOfTr) throws JsonGenerationException, JsonMappingException, IOExcepetion{
+	         ObjectMapper mapper = new ObjectMapper();
+	          mapper.writeValue(new File("arraylist.json"), arrayOfTr);
+        }
 
-		return cities.get(0);
-	}
-
-	public ArrayList<City> compare_cities(ArrayList<City> cities, int k) { // υπερφορτωση
-		Collections.sort(cities, new Sortbyres());
-		ArrayList<City> next = new ArrayList<City>();
-		for (int i = 2; i <= k; i++) {
-			next.add(cities.get(i));
-
+		public ArrayList<Traveller> readJSON() throws JsonParseException, JsonMappingException, IOException {
+			ObjectMapper mapper = new ObjectMapper();
+			ArrayList<Traveller> arrayOftr = mapper.readValue(new File("arraylist.json"), ArrayList.class);
+			return arrayOftr;
 		}
-		return next;
 
-	}
+		public long getTimestamp() {
+			return timestamp;
+		}
 
-	ArrayList<Traveller> travellers = new ArrayList<Traveller>();
-    
-	public double freeTicket(Object City) {
-		double max = travellers.get(0).calculate_similarity(City);
-		for (int i = 0; i < travellers.size(); i++) {
-			if (travellers.get(i).calculate_similarity(City) > max) { //πολυμορφισμος
-				max = travellers.get(i).calculate_similarity(City);
+		public void setTimestamp(long timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		protected abstract double calculate_similarity(Object City);
+
+		protected double similarity_geodesic_vector(double[] loc, double[] geodesic_vector, String unit) {
+			if ((loc[0] == geodesic_vector[0]) && loc[1] == geodesic_vector[1]) {
+				return 0;
+
+			} else {
+				double theta = loc[1] - geodesic_vector[1];
+				double distance = Math.sin(Math.toRadians(loc[0])) * Math.sin(Math.toRadians(geodesic_vector[0]))
+						+ Math.cos(Math.toRadians(loc[0])) * Math.cos(Math.toRadians(geodesic_vector[0]))
+								* Math.cos(Math.toRadians(theta));
+				distance = Math.acos(distance);
+				distance = Math.toDegrees(distance);
+				distance = distance * 60 * 1.1515;
+				if (unit.equals("K")) {
+					distance = distance * 1.609344;
+				} else if (unit.equals("N")) {
+					distance = distance * 0.8684;
+				}
+				return (distance);
 			}
 		}
-		return max;
-	}
 
+		public Object compare_cities(ArrayList<City> cities) {
+			Collections.sort(cities, new Sortbyres()); // ταξινομηση στο cities με βαση το similarity
+			System.out.println("\nSorted by similarity");
+			for (int i = 0; i < cities.size(); i++)
+				System.out.println(toString());
 
+			return cities.get(0);
+		}
+
+		public ArrayList<City> compare_cities(ArrayList<City> cities, int k) { // υπερφορτωση
+			Collections.sort(cities, new Sortbyres());
+			ArrayList<City> next = new ArrayList<City>();
+			for (int i = 2; i <= k; i++) {
+				next.add(cities.get(i));
+
+			}
+			return next;
+
+		}
+
+		ArrayList<Traveller> travellers = new ArrayList<Traveller>();
+
+		public double freeTicket(Object City) {
+			double max = travellers.get(0).calculate_similarity(City);
+			for (int i = 0; i < travellers.size(); i++) {
+				if (travellers.get(i).calculate_similarity(City) > max) { // πολυμορφισμος
+					max = travellers.get(i).calculate_similarity(City);
+				}
+			}
+			return max;
+		}
 
 }
